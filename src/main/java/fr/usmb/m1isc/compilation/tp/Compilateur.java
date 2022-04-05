83380e95681882074e9d44c0dee7c3916b5bb126
jubardt <dominiquejubard@gmail.com>
2022-04-05 14:34:09 +0200

    [ADD] jar ajouter dans le dossier compiledFiles

---
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Compilateur.java
@@ -0,0 +1,15 @@
+package fr.usmb.m1isc.compilation.tp;
+
+public class Compilateur {
+    private Arbre arbre;
+
+
+    public Compilateur(Arbre arbre){
+        this.arbre = arbre;
+    }
+
+    public void generateASM(String filename){
+        //Création de fichier
+
+    }
+}
